9ecedc1390379168d75d88879c34d9b6cce5c14f
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2021-09-08 20:32:58 -0500

    add/ new favicon :)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Jorge Arias Argüelles - Blog`,
+    title: `Jorge Arias Argüelles`,
     author: {
       name: `Jorge Arias Argüelles`,
       summary: `who lives in Cali, Colombia learning Web Developing.`,
@@ -118,7 +118,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
